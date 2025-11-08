b153d3239c41cf2654be912d31d97233af10b45b
Lina  Melendez <melendezvideslinadelcamen@gmail.com>
2025-11-08 18:21:10 -0500

    actualizado

--- routes/juego.js
+++ routes/juego.js
@@ -3,14 +3,21 @@ import Juego from '../models/Juego.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/buscar/:criterio', async (req, res) => {
   try {
-    const juegos = await Juego.find().sort({ fechaAgregado: -1 });
+    const juegos = await Juego.find({
+      $or: [
+        { titulo: { $regex: req.params.criterio, $options: 'i' } },
+        { genero: { $regex: req.params.criterio, $options: 'i' } }
+      ]
+    });
     res.json(juegos);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
+
+
 
 router.get('/:id', async (req, res) => {
   try {
@@ -22,15 +29,6 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
-  try {
-    const nuevoJuego = new Juego(req.body);
-    const guardado = await nuevoJuego.save();
-    res.status(201).json(guardado);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
 
 router.put('/:id', async (req, res) => {
   try {
@@ -52,14 +50,18 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
-router.get('/buscar/:criterio', async (req, res) => {
+router.post('/', async (req, res) => {
   try {
-    const juegos = await Juego.find({
-      $or: [
-        { titulo: { $regex: req.params.criterio, $options: 'i' } },
-        { genero: { $regex: req.params.criterio, $options: 'i' } }
-      ]
-    });
+    const nuevoJuego = new Juego(req.body);
+    const guardado = await nuevoJuego.save();
+    res.status(201).json(guardado);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+router.get('/', async (req, res) => {
+  try {
+    const juegos = await Juego.find().sort({ fechaAgregado: -1 });
     res.json(juegos);
   } catch (error) {
     res.status(500).json({ error: error.message });
